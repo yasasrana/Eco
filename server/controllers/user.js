10e4991a8d6215703c57c223df80d41e3f49e3d5
sathmijayasuriya <sathmijayasuriya@gmail.com>
2023-04-07 17:07:09 +0530

    user register

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -45,6 +45,51 @@ login: async (req, res) => {
   }
 },
 
+//user register 
+createUser: async (req, res) => {
+  try {
+    const { firstName, lastName, userName, role, email, password } = req.body;
+    const ExistingUser = await User.findOne({ email });
+    if (ExistingUser)
+      return res.status(400).json({
+        message:
+          "Someone has an account with the same email. Please use another email.",
+      });
+
+    if (!firstName || !lastName || !role || !userName || !password || !email)
+      return res.status(400).json({ msg: "Please fill in all fields." });
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      userName,
+      role,
+      email,
+      password: hashedPassword,
+    });
+    const account = await newUser.save();
+    if (account) {
+      res.status(200).json({
+        _id: account.id,
+        userName: account.userName,
+        role: account.role,
+        email: account.email,
+        token: generateToken(account._id),
+      });
+    } else {
+      return res.status(400).json({ msg: "Invalid user data" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+},
+
+
+
 };
 
 
