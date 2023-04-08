c55be149dcadb3672e887ca6a4102165c130d12b
PriyashanYPC <101043896+PriyashanYPC@users.noreply.github.com>
2023-04-08 10:44:15 +0530

    changes

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require ("dotenv").config();
+
+const app = express();
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use(cors())
+
+const PORT = process.env.PORT || 8000;
+const URL = process.env.MONGODB_URL;
+
+
+//Product management - chami
+const Product = require("./controller/AddProductController");
+app.use("/api", Product);
+
+//Appointment management - chami
+const Appointment = require("./controller/AddAppointmentController");
+app.use("/api", Appointment);
