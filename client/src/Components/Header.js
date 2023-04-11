bd24b5df4f2d1bdf6844cdd8d572deeb4efd73f1
sathmijayasuriya <sathmijayasuriya@gmail.com>
2023-04-11 20:18:40 +0530

    header of home page

---
+++ client/src/Components/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Logo from "../Images/LOGO.png";
+import Button from "@mui/material/Button";
+import { Box } from "@mui/material";
+
+
+const Header = () => {
+  const Styles = {
+    headerContainer: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
+    },
+    logo: {
+      display: "block",
+      margin: "auto",
+      width: "40px",
+    },
+    button : {
+      width: "auto",
+      mr:3,
+      color: "black"
+    }
+  };
+
+  return (
+    <header style={Styles.headerContainer}>
+      <img style={Styles.logo} src={Logo} alt="Logo" />
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          margin: "auto",
+          justifyContent: "center",
+          padding: "1px",
+        }}
+      >
+        <Button sx={Styles.button}>HOME</Button>
+        <Button sx={Styles.button}>MODELS</Button>
+        <Button sx={Styles.button}>ENERGY</Button>
+        <Button sx={Styles.button}>SUSTAINABILITY</Button>
+        <Button sx={Styles.button}>SUPPORT</Button>
+        <Button sx={Styles.button}>ABOUT</Button>
+      </Box>
+    </header>
+  );
+};
+
+export default Header;
